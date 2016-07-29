88b6279c9260dad60a893e1c6cee7809b7c80178
Karel Minarik <karmi@karmi.cz>
2016-07-29 17:03:42 +0200

    Updated the "Graph Explore" API URL
    Related: https://github.com/elastic/x-plugins/commit/a61e8c0

--- test/unit/graph/explore_test.rb
+++ test/unit/graph/explore_test.rb
@@ -10,7 +10,7 @@ module Elasticsearch
         should "perform correct request" do
           subject.expects(:perform_request).with do |method, url, params, body|
             assert_equal 'GET', method
-            assert_equal '_xpack/graph/_explore', url
+            assert_equal '_xpack/_graph/_explore', url
             assert_equal Hash.new, params
             assert_equal nil, body
             true
